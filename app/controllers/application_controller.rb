d38a75d14ebff6f1d2f74b470bc7d9351755dfb8
pierre <pdivvie@outlook.com>
2019-03-20 18:36:59 +0200

    Refactored devise_whitelist into a concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   include CurrentUserConcern
-
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
-  end
+  include DeviseWhitelist
 end
