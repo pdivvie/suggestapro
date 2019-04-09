bb081802e0ede3413774332a194547a34f61535f
pierre <pdivvie@outlook.com>
2019-04-09 12:43:39 +0200

    Installed Pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,6 @@ class ApplicationController < ActionController::Base
   include CurrentUserConcern
   include DeviseWhitelist
   include DefaultPageContent
+  include Pundit
+  protect_from_forgery
 end
