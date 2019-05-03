7a7014925c6a93da3a891166d06e26b81289328d
pierre <pdivvie@outlook.com>
2019-05-03 15:44:42 +0200

    Restyled the theme of the site to dark theme

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,9 +5,38 @@ module ApplicationHelper
       " ".html_safe +
       (link_to "Login", new_user_session_path,class: style)
     else
-      (link_to "Create a business page", new_business_path) +
+      (link_to "Create a business page", new_business_path,class: style) +
       " ".html_safe +
       (link_to "Logout", destroy_user_session_path, method: :delete,class: style) 
     end
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+      {
+        url: businesses_path,
+        title: 'Businesses'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
+
 end
