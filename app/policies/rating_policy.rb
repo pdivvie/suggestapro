68f196616bf7bac50ea765f41ff7b7ab55b049e9
pierre <pdivvie@outlook.com>
2019-05-03 17:26:44 +0200

    Implemented authorization for ratings to prevent users from editing and deleting reviews belonging to other users

---
+++ app/policies/rating_policy.rb
@@ -0,0 +1,16 @@
+class RatingPolicy < ApplicationPolicy
+  attr_reader :user, :rating
+
+  def initialize(user, rating)
+    @user = user
+    @rating = rating
+  end
+
+  def update?
+    (rating.user == user) || (user.has_role? :admin)
+  end
+
+  def destroy?
+    (rating.user == user) || (user.has_role? :admin)
+  end
+end
