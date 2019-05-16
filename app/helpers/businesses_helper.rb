5c6ad91e28ec679299ad3250a273b3a3563f73ca
pierre <pdivvie@outlook.com>
2019-05-16 12:46:56 +0200

    Refactored default images into a helper method

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -1,2 +1,14 @@
 module BusinessesHelper
+
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+
+  def business_img img
+    if img
+      img
+    else
+      image_generator(height: '480', width: '360')
+    end
+  end
 end
