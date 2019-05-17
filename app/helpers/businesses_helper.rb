6cad89dd7ce06aaa9754c78b5a0db574f6e49b06
pierre <pdivvie@outlook.com>
2019-05-17 12:01:20 +0200

    Changed placeholder image for business show page

--- app/helpers/businesses_helper.rb
+++ app/helpers/businesses_helper.rb
@@ -1,14 +1,10 @@
 module BusinessesHelper
 
-  def image_generator(height:, width:)
-    "http://placehold.it/#{height}x#{width}"
-  end
-
   def business_img img
     if img
       img
     else
-      image_generator(height: '480', width: '360')
+      "http://demo.warptheme.com/images/placeholder_600x400.svg"
     end
   end
 end
