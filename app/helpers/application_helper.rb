2602fc9f28b5d40b74a0b515c72e32244a2d1569
pierre <pdivvie@outlook.com>
2019-05-29 18:52:28 +0200

    commented out some redundant code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,47 +1,49 @@
 module ApplicationHelper
-  def login_helper style = ''
-    if current_user.is_a?(GuestUser)
-      (link_to "Register", new_user_registration_path,class: style) +
-      " ".html_safe +
-      (link_to "Login", new_user_session_path,class: style)
-    else
-      (link_to "Create a business page", new_business_path,class: style) +
-      " ".html_safe +
-      (link_to "My Services", my_services_path,class: style) +
-      " ".html_safe +
-      (link_to "My Reviews", ratings_path,class: style) +
-      " ".html_safe +
-      (link_to "Logout", destroy_user_session_path, method: :delete,class: style) 
-    end
-  end
+  ## commented code is redundant in current version. Keeping for now incase
 
-  def nav_items
-    [
-      {
-        url: root_path,
-        title: 'Home'
-      },
-      {
-        url: businesses_path,
-        title: 'Businesses'
-      }
-    ]
-  end
+  # def login_helper style = ''
+  #   if current_user.is_a?(GuestUser)
+  #     (link_to "Register", new_user_registration_path,class: style) +
+  #     " ".html_safe +
+  #     (link_to "Login", new_user_session_path,class: style)
+  #   else
+  #     (link_to "Create a business page", new_business_path,class: style) +
+  #     " ".html_safe +
+  #     (link_to "My Services", my_services_path,class: style) +
+  #     " ".html_safe +
+  #     (link_to "My Reviews", ratings_path,class: style) +
+  #     " ".html_safe +
+  #     (link_to "Logout", destroy_user_session_path, method: :delete,class: style) 
+  #   end
+  # end
 
-  def nav_helper style, tag_type
+  # def nav_items
+  #   [
+  #     {
+  #       url: root_path,
+  #       title: 'Home'
+  #     },
+  #     {
+  #       url: businesses_path,
+  #       title: 'Businesses'
+  #     }
+  #   ]
+  # end
 
-    nav_links = ''
+  # def nav_helper style, tag_type
 
-    nav_items.each do |item|
-      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
-    end
+  #   nav_links = ''
 
-    nav_links.html_safe
-  end
+  #   nav_items.each do |item|
+  #     nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+  #   end
 
-  def active? path
-    "active" if current_page? path
-  end
+  #   nav_links.html_safe
+  # end
+
+  # def active? path
+  #   "active" if current_page? path
+  # end
 
   def search_bar
     form_for "",url: businesses_path, role: "search", method: :get do
