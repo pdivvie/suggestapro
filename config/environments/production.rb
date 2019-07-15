a412ad37b37c073425da64eb6ba85d635f944f07
pierre <pdivvie@outlook.com>
2019-07-15 11:46:39 +0200

    Updated action_mailer smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { :host => 'https://pdv-rating-site.herokuapp.com' }
+  config.action_mailer.smtp_settings = { address: "https://pdv-rating-site.herokuapp.com" }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
