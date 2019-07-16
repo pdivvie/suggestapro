bd764c63884971240e948222438b288607e7f7c4
pierre <pdivvie@outlook.com>
2019-07-16 16:42:17 +0200

    Added the ability to update account details

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -11,6 +11,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :username, :email, :password, :password_confirmation, :location_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :username, :email, :password, :password_confirmation])
   end
 
 end
