bab9341b1d87999c9757d74f7c11add35b9070c1
pdivvie <pdivvie>
2019-07-19 14:33:32 +0200

    fixed a bug in the application nav and updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,29 +15,7 @@
 end
 
 1.times do
-  User.create!(
-    username: "pdivvie",
-    email: "pdivvie@outlook.com",
-    first_name: "Pierre",
-    last_name: "de Villiers",
-    password: "password",
-    location_id: 1
-  )
-end
-
-1.times do
   Category.create!(
     name: "Dentist",
   )
-end
-
-9.times do |business_item|
-  Business.create!(
-    name: "Business: #{business_item}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    user_id: 1,
-    category_id: 1,
-    location_id: 1
-  )
-
 end
