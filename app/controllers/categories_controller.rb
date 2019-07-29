8a11cd3bea8921aa5164a65d88e4f991ba9b39b6
pdivvie <pdivvie@outlook.com>
2019-07-29 15:30:40 +0200

    Updated routes to nest categories inside locations and added search categories function

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show, :edit, :update, :destroy]
+  before_action :set_location
 
   after_action :verify_authorized
+
+  layout "category"
 
   # GET /categories
   # GET /categories.json
@@ -40,7 +43,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Category was successfully created.' }
+        format.html { redirect_to [@location, @category], notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -56,7 +59,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Category was successfully updated.' }
+        format.html { redirect_to [@location, @category], notice: 'Category was successfully updated.' }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -72,7 +75,7 @@ class CategoriesController < ApplicationController
 
     @category.destroy
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to location_categories_url, notice: 'Category was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -81,6 +84,10 @@ class CategoriesController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_category
       @category = Category.find(params[:id])
+    end
+
+    def set_location
+      @location = Location.friendly.find(params[:location_id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
-  
-  resources :categories
+
+
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_up: 'register', sign_out: 'logout'}, controllers: { registrations: "registrations"}
 
-  
-  
+
+
   get 'faq', to: 'pages#faq'
-  
+
   resources :locations do
     get 'my-services', to: 'businesses#my_services'
     get 'my-reviews', to: 'ratings#my_reviews'
+    resources :categories
     resources :businesses do
       resources :ratings
     end
