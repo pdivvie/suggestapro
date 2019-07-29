aab182f1bdb94d6e7aa70246335d7b6f5ff14bb1
pdivvie <pdivvie@outlook.com>
2019-07-29 11:55:18 +0200

    Updated site name on tab

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -1,12 +1,12 @@
 module DefaultPageContent
   extend ActiveSupport::Concern
 
-  included do 
+  included do
     before_action :set_page_defaults
   end
 
   def set_page_defaults
-    @page_title = "Rating Site"
+    @page_title = "Suggest a Local Pro"
     @seo_keywords = "Rating, Business, Heldervue, Somerset West, Strand, Gordons Bay, Stellenbosch"
   end
 end
