0485ef93d33864dccf2f6e50b2152e97e9865739
pdivvie <pdivvie@outlook.com>
2019-08-19 13:21:11 +0200

    removed display_in_navbar variable from categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_094752) do
+ActiveRecord::Schema.define(version: 2019_08_19_111740) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_094752) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.boolean "display_in_navbar", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
