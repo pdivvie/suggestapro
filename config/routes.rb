7b4bac4852774748ed2bcb2cce0348ca548b93ae
pdivvie <pdivvie@outlook.com>
2019-08-29 12:53:44 +0200

    Reduced the width of suggestapro logo and moved about section from sidebar to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_up: 'register', sign_out: 'logout'}, controllers: { registrations: "registrations"}
 
   get 'legal-disclaimers', to: 'pages#legal_disclaimers'
+  get 'about', to: 'pages#about'
 
   resources :locations do
     get 'my-services', to: 'businesses#my_services'
