6d6a9db9158a5c71a211123443ceec76eefa8737
pdivvie <pdivvie@outlook.com>
2019-09-09 16:55:37 +0200

    Removed faq and news dropdown buttons from home page and set after sign out redirect to about page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     "/locations/#{current_user.location_id}/categories"
   end
 
+  def after_sign_out_path_for(resource)
+    "/about"
+  end
+
   def user_not_authorized
     flash[:alert] = "You are not authorized to perform this action."
     redirect_to(request.referrer || root_path)
